44b329670a7ee3d2b9d4fa583df4efefcc4ee0e2
Railan Deivid <contato.railandeivid@gmail.com>
2024-08-12 08:26:54 -0300

    update

---
+++ Desenvolvimento_web/Aulas/JS/area.js
@@ -0,0 +1,9 @@
+let raio = prompt("Insira o raio do circulo");
+
+raio = parseFloat(raio);
+
+const area = Math.PI*Math.pow(raio,2);
+
+console.log(`A area de circulo é :` , area);
+
+alert("A area do circulo é: "+area.toFixed(2))
